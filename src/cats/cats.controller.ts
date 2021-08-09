1b491767e44f8c4faa373eaafd95ff1bd3e324c5
Hanna Lee <8annahxxl@gmail.com>
2021-08-09 16:31:28 +0900

    예외 필터 추가

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,25 @@
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly CatsService: CatsService) {}
 
   @Get()
   getAllCat() {
-    return 'all cat';
+    throw new HttpException('api is broken', 401);
+    // return 'get all cat'
   }
 
   @Get(':id')
