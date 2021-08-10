8a7950fb77fecc9b3ae362716ad83b9d8e2cac85
Hanna Lee <8annahxxl@gmail.com>
2021-08-10 21:32:39 +0900

    Hot reload 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  imports: [CatsModule],
+  imports: [ConfigModule.forRoot(), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(8000);
+
+  const PORT = process.env.PORT || 4000;
+  await app.listen(PORT, () => {
+    console.log(`🚀 Listening on http://localhost:${PORT}/`);
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
