59b5d81729e084a7ed60c8c661ed47ec57e99cfe
Hanna Lee <8annahxxl@gmail.com>
2021-08-11 15:41:21 +0900

    Repository 패턴 적용하여 리팩토링

---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,20 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { HttpException, Injectable } from '@nestjs/common';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existsByEmail(email: string): Promise<boolean> {
+    // 현재 validation 적용으로 에러 처리가 잘 되기 때문에 try-catch 구문 생략
+    const result = await this.catModel.exists({ email });
+    return result;
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
