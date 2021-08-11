cc4d532a11ce147ffad257addad22dded9102f68
Hanna Lee <8annahxxl@gmail.com>
2021-08-11 17:09:09 +0900

    로그인 서비스 구현

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { CatsModule } from 'src/cats/cats.module';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt/jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1y' },
+    }),
+    forwardRef(() => CatsModule), // 순환 종속성 해결
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
--- src/cats/cats.repository.ts
+++ src/cats/cats.repository.ts
@@ -1,12 +1,17 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cat } from './cats.schema';
 import { CatRequestDto } from './dto/cats.request.dto';
 
 @Injectable()
 export class CatsRepository {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async findCatByEmail(email: string): Promise<Cat | null> {
+    const cat = await this.catModel.findOne({ email });
+    return cat;
+  }
 
   async existsByEmail(email: string): Promise<boolean> {
     // 현재 validation 적용으로 에러 처리가 잘 되기 때문에 try-catch 구문 생략
